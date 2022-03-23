e2617b08834fc099f4eba6e0a35681efe2778266
Harsh Vardhan Tandon <hvt1998@gmail.com>
2022-03-23 14:24:23 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// rafce
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+    </>
+  )
+}
+
+export default Header
+© 2022 GitHub, Inc.
+Terms
